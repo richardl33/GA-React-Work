d2f91820ea57c0a04d889149f4e94933cf55305d
Richard Lee <richard.lee@groupeconnect.com>
2020-12-15 15:00:37 -0500

    function component & functional component shorthand

--- todo-list/src/ListItem.js
+++ todo-list/src/ListItem.js
@@ -1,14 +1,27 @@
 import React, { Component } from "react";
 import "./App.css";
 
-class ListItem extends Component {
-  render() {
-    return (
-      <div>
-        <li>{this.props.todoItem}</li>
-      </div>
-    );
-  }
-}
+// Class Component
+// class ListItem extends Component {
+//   render() {
+//     return (
+//       <div>
+//         <li>{this.props.todoItem}</li>
+//       </div>
+//     );
+//   }
+// }
+
+// Functional Component
+// const ListItem = (props) => {
+//    return (
+//       <div>
+//          <li>{props.todoItem}</li>
+//       </div>
+//    )
+// }
+
+// Functional Component - Shorthand
+const ListItem = props => <li>{props.todoItem}</li>;
 
 export default ListItem;
