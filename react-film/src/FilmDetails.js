49d706b30331ed24ee305b96d618d5f56c98c83e
Richard Lee <richard.lee@groupeconnect.com>
2020-12-14 21:58:27 -0500

    film hw

---
+++ react-film/src/FilmDetails.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+class FilmDetails extends Component {
+   render() {
+      return (
+         <div></div>
+      )
+   }
+}
+
+export default FilmDetails;
---
+++ react-film/src/FilmListing.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class FilmListing extends Component {
+   render() {
+      return (
+         <div></div>
+      )
+   }
+}
+
+export default FilmListing;
